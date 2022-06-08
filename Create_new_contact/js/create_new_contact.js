1a6c808700005745e7f1dce96f8dfd469ca2288b
MariSakanyan <merisakanyan99@gmail.com>
2022-06-09 00:29:10 +0400

    Phone book not finished

---
+++ Create_new_contact/js/create_new_contact.js
@@ -0,0 +1,9 @@
+document.querySelector('#submit').onclick = function() {
+    let name = document.querySelector('#name').value;
+    let surname = document.querySelector('#surname');
+    let number = document.querySelector('#number');
+    let email = document.querySelector('#email');
+    let message = document.querySelector('#message');
+
+    console.log(name);
+}
---
+++ Login_page/js/login_page.js
@@ -0,0 +1,23 @@
+document.querySelector('#check').onclick = function(){
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    if(email == 'sakanyan-meri@mail.ru' && password == 'Sakanyan') {
+        document.location.href = '..//user_page/user_page.html';
+    }
+    else if (email.length > 30 || password.length < 8 || password.length > 10){
+        let error = 'incorrect length of '
+         if(email.length > 30){
+             error = error + 'email';
+        }
+         if (password.length < 8 || password.length > 10){
+            error = error + ' password';
+        }
+        document.querySelector('.error_message').append(error);
+    }
+   
+    else {
+        // alert('incorrect login or password!');
+         document.querySelector('.error_message').append('Incorrect login or password!');
+    }
+}
